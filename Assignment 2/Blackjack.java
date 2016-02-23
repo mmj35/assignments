d170305472702b1749e91821c59a8a9188b132cf
Melody Jolly <mmj35@pitt.edu>
2016-02-23 14:07:58 -0500

    message

--- Assignment 2/Blackjack.java
+++ Assignment 2/Blackjack.java
@@ -1,12 +1,19 @@
 import java.util.Scanner; 
-public static void BlackJack { 
+public class BlackJack { 
 	public static void main (String [] args) {
 		Scanner keyboard= new Scanner(System.in);
 	
 		System.out.println("Enter name> ");
-		string name= keyboard.nextLine(); 
+		String name= keyboard.nextLine(); 
 		System.out.print("Welcome, " + name + "!");
-		System.out.print("Have fun on your first time playing Infinite Blackjack.")
+		System.out.print("Have fun on your first time playing Infinite Blackjack.");
+		
+		
+		Player user= new Player(name, 0, 0, 100);
+		
+		System.out.print("Name:                " + name);
+		System.out.print("Total Hands won             0");
+		System.out.print("Money:                      0");                 
 	
 	}
 }
---
+++ Assignment 2/Player.java
@@ -0,0 +1,16 @@
+public class Player {
+	
+		String name;
+		int hands;
+		int handsWon;
+		int Money;
+	
+		public Player(String name, int hands,int handsWon,int Money) {
+			this.name =name;
+			this.hands=hands;
+			this.handsWon=handsWon;
+			this.Money=Money;	
+		}
+	}
+	
+	
